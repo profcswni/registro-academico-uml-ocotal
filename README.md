03cc23fa0c41e81d2c8ef9c574ae248bf2ca2932
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2022-12-17 12:52:33 -0600

    Plantilla de app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # registro-academico-uml-ocotal
-Base de datos en Firestore
+Base de datos en Firestore-
---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA3odJJcJhxDvDJIpEPoRK9BJyHyXMYJO8",
+    authDomain: "registro-academico-uml-ocotal.firebaseapp.com",
+    projectId: "registro-academico-uml-ocotal",
+    storageBucket: "registro-academico-uml-ocotal.appspot.com",
+    messagingSenderId: "180649277801",
+    appId: "1:180649277801:web:88322f061ffe0a3acbe5f8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
